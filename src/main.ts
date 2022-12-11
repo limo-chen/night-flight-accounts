747e36d9014fb062f73693d3684b3aab3176e88f
jinglu <2320035651@qq.com>
2022-12-11 20:53:16 +0800

    重构main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { createApp } from 'vue'
 import { App } from './App';
-import { Foo } from './views/Foo';
-import { Bar } from './views/Bar';
-import {createRouter,createWebHashHistory} from "vue-router"//把组件改成单独导出，这样就可以减少代码体积
+import {createRouter} from "vue-router"
+import { routes } from './config/routes';
+import { history } from './shared/history';
 
-const routes = [
-    {path:'/',component:Foo},
-    {path:'/about',component:Bar},
-]
+
 
 const router =createRouter({
-    history: createWebHashHistory(),//hash模式
-    routes,//将第二步的routes放在这里
+    history,
+    routes,
   })
 
-const app= createApp(App) //app先创建
-app.use(router) //use router
-app.mount('#app')//app再挂载
+const app= createApp(App) 
+app.use(router)
+app.mount('#app')
