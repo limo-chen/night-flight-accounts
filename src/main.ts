a95e5187797586b2862950c177a1b6626d112424
jinglu <2320035651@qq.com>
2022-12-11 15:59:18 +0800

    引入 vue-router4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import { App } from './App';
+import { Foo } from './views/Foo';
+import { Bar } from './views/Bar';
+import {createRouter,createWebHashHistory} from "vue-router"//把组件改成单独导出，这样就可以减少代码体积
 
-createApp(App).mount('#app')
+const routes = [
+    {path:'/',component:Foo},
+    {path:'/about',component:Bar},
+]
+
+const router =createRouter({
+    history: createWebHashHistory(),//hash模式
+    routes,//将第二步的routes放在这里
+  })
+
+const app= createApp(App) //app先创建
+app.use(router) //use router
+app.mount('#app')//app再挂载
