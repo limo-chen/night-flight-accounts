1bd0a9af104c82b66f1fcbb4fb9a06dd8e6f023f
jinglu <2320035651@qq.com>
2022-12-12 21:34:24 +0800

    第一个欢迎界面

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,20 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from "./Welcome.module.scss";
+import logo from "../assets/icons/plane.svg";
+console.log(logo);
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <RouterView />
+      <div class={s.wrapper}>
+        <header>
+          <img src={logo} />
+          <h1>Spend Travel</h1>
+        </header>
+        <main class={s.main}>
+          <RouterView />
+        </main>
       </div>
     );
   },
