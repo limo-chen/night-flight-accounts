d2f20a342459a0ebff21a3a346709d55657afd69
jinglu <2320035651@qq.com>
2023-02-03 11:31:24 +0800

    优化了，但是代码行数变多了

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import {createRouter} from "vue-router"
 import { routes } from './config/routes';
 import { history } from './shared/history';
 import '@svgstore';
-import { http } from './shared/Http';
 import { fetchMe, mePromise } from './shared/me';
 
 
@@ -15,18 +14,29 @@ const router = createRouter({
 
   fetchMe()
 
-  router.beforeEach(async (to, from) => {
-    if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')
-      || to.path === '/start') {
-      return true
-    } else {
-      const path = await mePromise!.then(
-        () => true,
-        () => '/sign_in?return_to=' + to.path
-      )
-      return path
+  const whiteList: Record<string, 'exact' | 'startsWith'> = {
+    '/': 'exact',
+    '/start': 'exact',
+    '/welcome': 'startsWith',
+    '/sign_in': 'startsWith',
+  }
+  
+  router.beforeEach((to, from) => {
+    for (const key in whiteList) {
+      const value = whiteList[key]
+      if (value === 'exact' && to.path === key) {
+        return true
+      }
+      if (value === 'startsWith' && to.path.startsWith(key)) {
+        return true
+      }
     }
-  })
+    return mePromise!.then(
+      () => true,
+      () => '/sign_in?return_to=' + to.path
+    )
+    }
+  )
 
 const app= createApp(App) 
 app.use(router)
