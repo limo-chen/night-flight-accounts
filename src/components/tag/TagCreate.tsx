551b706c974b6c02a5bae783e5bf0fd9942eddcc
jinglu <2320035651@qq.com>
2023-02-07 16:04:06 +0800

    创建标签

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,43 +1,10 @@
-import { defineComponent, PropType, reactive, toRaw } from "vue";
+import { defineComponent } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import { Button } from "../../shared/Button";
-import { Icon } from "../../shared/Icon";
-import { Rules, validate } from "../../shared/validate";
-import { EmojiSelect } from "../../shared/EmojiSelect";
 import { TagForm } from "./TagForm";
-import s from "./Tag.module.scss";
 import { BackIcon } from "../../shared/BackIcon";
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>,
-    },
-  },
   setup: (props, context) => {
-    const formData = reactive({
-      name: "",
-      sign: "",
-    });
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
-    const onSubmit = (e: Event) => {
-      const rules: Rules<typeof formData> = [
-        { key: "name", type: "required", message: "必填" },
-        {
-          key: "name",
-          type: "pattern",
-          regex: /^.{1,4}$/,
-          message: "只能填 1 到 4 个字符",
-        },
-        { key: "sign", type: "required", message: "必填" },
-      ];
-      Object.assign(errors, {
-        name: undefined,
-        sign: undefined,
-      });
-      Object.assign(errors, validate(formData, rules));
-      e.preventDefault();
-    };
     return () => (
       <MainLayout>
         {{
